749225580286ca752d6fabb2d9857e4d18efa98a
shanfei <acesse@shannons-MacBook-Pro.local>
2013-10-07 15:12:57 -0700

    init the rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedeemRails::Application.config.secret_key_base = 'ea07fcf845334d64d9bc1f944dd4674f5d8655887d2298dbf4a9d9e666a7937645e64631271838cc493c4f36f90b5a166fd49befdb72d71a89634ade019b18dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RedeemRails::Application.config.session_store :cookie_store, key: '_RedeemRails_session'
